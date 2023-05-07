705acd25b76bc929f426ee74f003c17ebb94940f
Anton Bushuiev <anton.bushuiev@gmail.com>
2023-05-07 14:49:02 +0200

    Rmove ._ files

--- data/get_skempi_v2.sh
+++ data/get_skempi_v2.sh
@@ -3,3 +3,4 @@ cd SKEMPI_v2
 wget https://life.bsc.es/pid/skempi2/database/download/skempi_v2.csv
 wget https://life.bsc.es/pid/skempi2/database/download/SKEMPI2_PDBs.tgz
 tar -xzvf SKEMPI2_PDBs.tgz
+rm -rf PDBs/._*
